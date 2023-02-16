745c1b04c69fbbfe45c57a1cd6408e13ecfb961f
Th1ng0 <thingotm@gmail.com>
2023-02-16 15:09:21 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printf function
+ * Return: 0 (Success)
+ */
+ * int main(void)
+{puts("\"with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
