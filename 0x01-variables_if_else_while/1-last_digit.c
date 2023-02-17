173a8d98362c40267290cfdc73839b34de71849f
Th1ng0 <thingotm@gmail.com>
2023-02-17 15:33:55 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 printf("%d ", n);
 if (n > 0)
-printf("is positive 5\n", n, last);
+printf("is positive 5\n");
 else if (n == 0)
-printf("is zero 0\n"), n last);
+printf("is zero 0\n");
 else
-printf("is negative 0\n", n, last);
+printf("is negative 0\n");
 return (0);
 }
