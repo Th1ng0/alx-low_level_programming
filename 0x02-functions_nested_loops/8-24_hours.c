c8ea4997e04b156e3f24e67bf08c616c8fb6014a
Th1ng0 <thingotm@gmail.com>
2023-02-21 16:18:31 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,41 +1,30 @@
 #include "main.h"
-
 /**
- * times_table - check description
- * Description: It prints 9 times table starting with 0
- * Return: Nothing.
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * starting from 00:00 to 23:59
+ *
  */
-void times_table(void)
+void jack_bauer(void)
 {
-	int i, j, n;
+	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	i = 0;
+
+	while (i < 24)
 	{
-		for (j = 0; j <= 9; j++)
+		j = 0;
+
+		while (j < 60)
 		{
-			n = i * j;
-
-			if ((n / 10) == 0)
-			{
-				if (j != 0)
-					_putchar(' ');
-				_putchar(n + '0');
-
-				if (j == 9)
-					continue;
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				if (j == 9)
-					continue;
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
 		}
-		_putchar('\n');
+		i++;
 	}
 }
--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-	times_table();
+	jack_bauer();
 	return (0);
 }
-
